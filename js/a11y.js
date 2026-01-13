5d7c338524315781b681042e337879fad7d3bd23
deltamarket015 <deltamarket015@gmail.com>
2026-01-13 22:54:59 +0530

    Delete js directory

--- js/a11y.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("keydown", e => {
-    if (e.key === "Escape") {
-        const s = document.getElementById("productSearch");
-        if (s) s.value = "";
-    }
-});
--- js/skeleton.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll(".product-card");
-    cards.forEach(c => c.classList.add("skeleton"));
-
-    window.addEventListener("load", () => {
-        cards.forEach(c => c.classList.remove("skeleton"));
-    });
-});
