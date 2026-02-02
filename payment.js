86a857a1db9aea1dd276324af929bc144b71fdc0
deltamarket015 <deltamarket015@gmail.com>
2026-02-02 21:59:32 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,4 +1,4 @@
-const API = "https://backend-production-2cbc.up.railway.app";
+const API = "https://api.deltamarket.store";
 const VALID_REFERRALS = ["REIO50", "SHU50", "FLASH50"];
 
 let cache = {};
@@ -144,3 +144,4 @@ function goPlatform() {
   window.location.href =
     `https://t.me/${TELEGRAM_USERNAME}?text=` + encodeURIComponent(msg);
 }
+
